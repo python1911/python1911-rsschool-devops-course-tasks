07f7e7af3ed558b9d0d5817860b2602c095a46fb
Irakli Pipia <4u3v2w1@gmail.com>
2025-06-13 01:36:47 +0400

    feat: implement GitHub OIDC role with Terraform

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,15 @@
 data "aws_caller_identity" "current" {}
 
+# 1. OIDC Provider for GitHub Actions
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+# 2. IAM Role for GitHub Actions
 resource "aws_iam_role" "github_actions_role" {
   name = "GithubActionsRole"
 
@@ -9,7 +19,7 @@ resource "aws_iam_role" "github_actions_role" {
       {
         Effect = "Allow",
         Principal = {
-          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+          Federated = aws_iam_openid_connect_provider.github.arn
         },
         Action = "sts:AssumeRoleWithWebIdentity",
         Condition = {
